6626dea02c72f5d867d5ea8f76134389ec93bf37
jalandra <jalandra@users.noreply.github.com>
2020-10-04 16:30:07 +0530

    Update min_jumps_to_reach_end.cpp

--- min_jumps_to_reach_end.cpp
+++ min_jumps_to_reach_end.cpp
@@ -21,7 +21,6 @@ int minJumps(vector<int> &arr, int index, int end, int jumps) {
     }
     return jumps_min;   
 }
-//DP
 int main()
 {
  //arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}	//code
@@ -42,6 +41,73 @@ int main()
     else
         cout<<minJumps(arr, 0, arr[0], 0)<<endl;
     arr.clear();
+ }
+ return 0;
+}
+///DP
+/DP
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+int minJumps(vector<int> &arr, int index, int end, int jumps, vector<int> &jumps_dp) {
+    static int jumps_min = INT_MAX;
+    if(arr[index] == 0) {
+        return jumps_dp[index];
+    }
+    else if(index + arr[index] >= arr.size() -1) {
+        jumps_dp[index] = jumps+1;
+        return jumps_dp[index];
+    }
+    
+    for(int i=index+1; i<=end; ++i) {
+        if(jumps_dp[i] != INT_MAX && jumps_dp[i] < jumps_dp[index]) {
+            jumps_dp[i] = jumps_dp[index];
+        }
+        else {
+            jumps_dp[i] = minJumps(arr, i, i+arr[i], jumps+1, jumps_dp);
+        }
+        if(jumps_dp[index] > jumps_dp[i])
+            jumps_dp[index] = jumps_dp[i];
+    }
+    /*for(int i=0; i<arr.size();++i)
+        cout<<jumps_dp[i]<<" ";
+    cout<<end;*/
+    return jumps_dp[index];
+}
+
+int main()
+{
+ //arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}	//code
+ int t;
+ cin>>t;
+ while(t--) {
+    int n;
+    cin>>n;
+    vector<int> arr;
+    vector<int> jumps_dp;
+    arr.reserve(n);
+    jumps_dp.reserve(n);
+    int elem;
+    while(n--) {
+        cin>>elem;
+        arr.push_back(elem);
+        jumps_dp.push_back(INT_MAX);
+    }
+    if(arr[0] == 0)
+        cout<<"-1"<<endl;
+    else {
+        //jumps_dp[0] = 1;
+        cout<<minJumps(arr, 0, arr[0], 0, jumps_dp)<<endl;
+        //cout<<jumps_dp[0]<<endl;
+    }
+    /*cout<<"jumps: ";
+    for(int i=0; i<arr.size();++i)
+        cout<<jumps_dp[i]<<" ";
+    */
+    arr.clear();
+    jumps_dp.clear();
  }
  return 0;
 }
